65838d300f7571457c722c1d1070f75a7540331d
Sreexcerpt <sree.excerpt@gmail.com>
2025-03-01 12:45:33 +0530

    header

--- caddesk/src/Components/Header/Header.jsx
+++ caddesk/src/Components/Header/Header.jsx
@@ -7,11 +7,15 @@ const Header = () => {
         {/* <!-- Logo --> */}
         <div className="header-left active">
           <a href="index.html" className="logo logo-normal">
-          <img src="./src/assets/assets/img/cad.png" className="img-fluid" alt="Profile" />
-          <img src="./src/assets/assets/img/cad.png" className="img-fluid" alt="Profile" />
+            <img src="./src/assets/assets/img/cad.png" alt="Logo" />
+            <img
+              src="./src/assets/assets/img/white-logo.svg"
+              className="white-logo"
+              alt="Logo"
+            />
           </a>
           <a href="index.html" className="logo-small">
-          <img src="./src/assets/assets/img/cad.png" className="img-fluid" alt="Profile" />
+            <img src="assets/img/logo-small.svg" alt="Logo" />
           </a>
           <a id="toggle_btn" href="javascript:void(0);">
             <i className="ti ti-arrow-bar-to-left"></i>
